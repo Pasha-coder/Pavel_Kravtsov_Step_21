3481cf9ba3bd470b75462141bc7743e31e76ac5b
Pasha-coder <pavelkravtsov99@gmail.com>
2021-08-07 20:52:20 +0300

    completed task-1

--- Coding_homework_3/task-1/index-1.js
+++ Coding_homework_3/task-1/index-1.js
@@ -1,11 +1,13 @@
-let age = +prompt('Введите ваш возраст');
+let firstNumber = +prompt('Введите число');
+let secondNumber = +prompt('Введите число');
+let start = firstNumber > secondNumber ? secondNumber : firstNumber;
+let end = firstNumber < secondNumber ? secondNumber : firstNumber;
+let result = 0;
 
-if (age > 0 && age < 12) {
-    alert('Вы ребёнок');
-} else if (age >= 12 && age < 18) {
-    alert('Вы подросток')
-} else if (age >= 18 && age < 60) {
-    alert('Вы взрослый');
-} else {
-    alert('Вы пенсионер');
-};
+
+while (start <= end) {
+    result = result + start;
+    start++;
+}
+
+alert(result);
