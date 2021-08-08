765ccc1c7bbc9d16363f5cae0162259737c59d53
Pasha-coder <pavelkravtsov99@gmail.com>
2021-08-08 18:15:03 +0300

    сщьздуеув ефыл-10

--- Coding_homework_2/task-10/index-10.js
+++ Coding_homework_2/task-10/index-10.js
@@ -4,55 +4,33 @@ let year = +prompt('Введите год');
 
 
 if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
-    if (day >= 1 && day <= 30) {
-        day += 1;
+    if (month === 12 && day === 31) {
+        alert(`1.1.${year + 1}`);
+    } else if (day >= 1 && day <= 30) {
+        alert(`${day + 1}.${month}.${year}`);
     } else if (day === 31) {
-        day = 1;
-        month += 1;
-    } else {
-        alert('Ошибка ввода');
-    }
-};
-
-if (month === 4 || month === 6 || month === 9 || month === 11) {
+        alert(`1.${month + 1}.${year}`);
+    };
+} else if (month === 4 || month === 6 || month === 9 || month === 11) {
     if (day >= 1 && day <= 29) {
-        day += 1;
+        alert(`${day + 1}.${month}.${year}`);
     } else if (day === 30) {
-        day = 1;
-        month += 1;
-    } else {
-        alert('Ошибка ввода');
-    }
-};
-
-if (month === 2) {
+        alert(`1.${month + 1}.${year}`);
+    };
+} else if (month === 2) {
     if (year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
         if (day >= 1 && day <= 28) {
-            day += 1;
+            alert(`${day + 1}.${month}.${year}`);
         } else if (day === 29) {
-            day = 1;
-            month += 1;
-        } else {
-            alert('Ошибка ввода');
+            alert(`1.${month + 1}.${year}`);
         };
     } else if (year % 400 !== 0 || year % 4 !==0 && year % 100 !==0 || year % 4 === 0 && year % 100 === 0) {
         if (day >= 1 && day <= 27) {
-        day += 1;
+            alert(`${day + 1}.${month}.${year}`);
         } else if (day === 28) {
-        day = 1;
-        month += 1;
-        } else {
-        alert('Ошибка ввода');
-        }
+            alert(`1.${month + 1}.${year}`);
+        };
     };
 };
 
-if (day === 31 && month === 12) {
-    day = 1;
-    month = 1;
-    year += 1;
-} else {
-    alert('Ошибка ввода');
-};
 
-alert(`${day}.${month}.${year}`);
