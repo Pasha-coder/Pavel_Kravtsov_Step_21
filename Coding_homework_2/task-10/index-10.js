df3639af3a56943f016aca8a93cae875f75f3580
Pasha-coder <pavelkravtsov99@gmail.com>
2021-08-11 16:31:35 +0300

    corrected last commit

--- Coding_homework_2/task-10/index-10.js
+++ Coding_homework_2/task-10/index-10.js
@@ -2,7 +2,6 @@ let day = +prompt('Введите день');
 let month = +prompt('Введите месяц');
 let year = +prompt('Введите год');
 
-
 if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
     if (month === 12 && day === 31) {
         alert(`1.1.${year + 1}`);
