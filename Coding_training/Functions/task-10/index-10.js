5cc08ec166445dd105083185c8cc35358b7f222e
Pasha-coder <pavelkravtsov99@gmail.com>
2021-08-12 20:51:19 +0300

    completed task-10

---
+++ Coding_training/Functions/task-10/index-10.js
@@ -0,0 +1,38 @@
+function isLeap(day, month, year) {
+    if (year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
+        if (day >= 1 && day < 29) {
+            return `${day++}.${month}.${year}`;
+        } else if (day === 29) {
+            return `1.${month++}.${year}`;
+        };
+    } else {
+        if (day >= 1 && day < 28) {
+            return `${day++}.${month}.${year}`;
+        } else if (day === 28) {
+            return `1.${month++}.${year}`;
+        };
+    };
+} 
+
+function nextDay(day, month, year) {
+    if (day === 31 && month === 12) {
+        return `1.1.${++year}`;
+    };
+    if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
+        if (day >= 1 && day < 31) {
+            return `${day++}.${month}.${year}`;
+        } else if (day === 31) {
+            return `1.${month++}.${year}`;
+        };
+    } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+        if (day >= 1 && day < 29) {
+            return `${day++}.${month}.${year}`;
+        } else if (day === 30) {
+            return `1.${month++}.${year}`; 
+        };
+    } else if (month === 2) {
+        isLeap(day, month, year);
+    };
+}
+
+console.log(nextDay(31, 12, 2020));
