926fe1421bbba562838db83e9de0280c13dc5682
Pasha-coder <pavelkravtsov99@gmail.com>
2021-08-26 00:11:53 +0300

    minor changes in task-1

--- Coding_homework_5/task-1/index-1.js
+++ Coding_homework_5/task-1/index-1.js
@@ -2,7 +2,7 @@ let car = {
     manufacturer: 'Россия',
     model: 'ВАЗ Нива',
     yearOfManufacture: '2008',
-    averageSpeed: 100,
+    averageSpeed: 90,
 };
 
 function showInfo(object) {
@@ -13,7 +13,7 @@ function howLong(object, distance) {
     let time = distance / object.averageSpeed;
     let stop = 4;
     let count = 0;
-    while(time - stop >= 0) {
+    while(time - stop > 0) {
         stop += 4;
         count++;
     };
