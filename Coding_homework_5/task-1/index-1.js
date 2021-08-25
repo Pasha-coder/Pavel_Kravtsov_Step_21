eed0cea1527d5cf42a0193923dcf8dd592a69abe
Pasha-coder <pavelkravtsov99@gmail.com>
2021-08-26 00:08:32 +0300

    completed task-1

---
+++ Coding_homework_5/task-1/index-1.js
@@ -0,0 +1,22 @@
+let car = {
+    manufacturer: 'Россия',
+    model: 'ВАЗ Нива',
+    yearOfManufacture: '2008',
+    averageSpeed: 100,
+};
+
+function showInfo(object) {
+    console.log(object);
+};
+
+function howLong(object, distance) {
+    let time = distance / object.averageSpeed;
+    let stop = 4;
+    let count = 0;
+    while(time - stop >= 0) {
+        stop += 4;
+        count++;
+    };
+    return time + count;
+}
+
