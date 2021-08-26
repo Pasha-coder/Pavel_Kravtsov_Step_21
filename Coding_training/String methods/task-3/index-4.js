84644ec643bb85410f841ec89f8186e9c4defc05
Pasha-coder <pavelkravtsov99@gmail.com>
2021-08-26 20:26:40 +0300

    сщьздуеув ефыл-4

---
+++ Coding_training/String methods/task-3/index-4.js
@@ -0,0 +1,15 @@
+let spam = ['100% бесплатно', 'увеличение продаж', 'только сегодня', 'не удаляйте'];
+
+function isHereSpam(string) {
+    let foundSpam;
+    let newString = string.toLowerCase();
+        for (let n = 0; n < spam.length; n++) {
+           foundSpam = newString.includes(spam[n]);
+           if (foundSpam === true) {
+               break;
+           }
+        }; 
+    return foundSpam;
+};
+
+console.log(isHereSpam(' мамай клянус уВЕлИЧение ПРОдаЖ!!!'));
