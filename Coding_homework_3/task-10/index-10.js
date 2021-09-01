86b260486e608e9cd58d8fb83d30b0a2153bb0d1
Pasha-coder <pavelkravtsov99@gmail.com>
2021-09-01 13:32:47 +0300

    completed task-10

--- Coding_homework_3/task-10/index-10.js
+++ Coding_homework_3/task-10/index-10.js
@@ -1,4 +1,21 @@
+let start = 0;
+let end = 100;
+let num2 = false;
 
+alert(`Задумайте число от ${start} до ${end}`);
+
+while (num2 == false) {
+    let num1 = Math.round((start + end) / 2);
+    let answer = prompt(`Число <, > or == ${num1}`);
+    if (answer === '==') {
+        alert('true');
+        break;
+    } else if (answer === '<'){
+        end = num1;
+    } else if (answer === '>'){
+        start = num1;
+    };
+};
 
 
 
