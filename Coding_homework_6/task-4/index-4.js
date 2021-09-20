22421edb7e79ffb75be27e145a339cb95f90d7b0
Pasha-coder <pavelkravtsov99@gmail.com>
2021-09-20 19:33:28 +0300

    completed task-4

--- Coding_homework_6/task-4/index-4.js
+++ Coding_homework_6/task-4/index-4.js
@@ -3,7 +3,7 @@ let academy = [
     {classroomNumber: 'A', numberOfSeats: 17, facultyName: 'Marketing'},
     {classroomNumber: 'B', numberOfSeats: 19, facultyName: 'Management'},
     {classroomNumber: 'E', numberOfSeats: 16, facultyName: 'International Relationships'},
-    {classroomNumber: 'D', numberOfSeats: 19, facultyName: 'Phylosophy'}
+    {classroomNumber: 'D', numberOfSeats: 10, facultyName: 'Philosophy'}
 ];
 
 function showAllClassrooms(array) {
@@ -15,3 +15,29 @@ function showClassroomForTheFaculty(array, faculty) {
     return classroom.classroomNumber;
 };
 
+function showClassroomForTheGroup(array, name, number, faculty) {
+    let group = {groupName: name, numberOfStudents: number, facultyOfGroup: faculty};
+    if (array.find(item => item.numberOfSeats >= group.numberOfStudents && item.facultyName === group.facultyOfGroup) === undefined) {
+        return 'There are no available classrooms for this group';
+    } else {
+    return array.find(item => item.numberOfSeats >= group.numberOfStudents && item.facultyName === group.facultyOfGroup); 
+    };
+};
+
+function sortClassroomsByNumberOfSeats(array) {
+    function compareClassroomsNumberOfSeats(a, b) {
+        if (a.numberOfSeats > b.numberOfSeats) return 1;
+        if (a.numberOfSeats === b.numberOfSeats) return 0;
+        if (a.numberOfSeats < b.numberOfSeats) return -1;
+    };
+    return array.sort(compareClassroomsNumberOfSeats);
+}
+
+function sortClassroomsByTheirNames(array) {
+    function compareClassroomsNames(a, b) {
+        if (a.classroomNumber > b.classroomNumber) return 1;
+        if (a.classroomNumber === b.classroomNumber) return 0;
+        if (a.classroomNumber < b.classroomNumber) return -1;
+    };
+    return array.sort(compareClassroomsNames);
+};
