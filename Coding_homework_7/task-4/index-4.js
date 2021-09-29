2961a064a29d8deeaa4164d9d988b9e96495ca4e
Pasha-coder <pavelkravtsov99@gmail.com>
2021-09-29 18:24:27 +0300

    completed task-4

---
+++ Coding_homework_7/task-4/index-4.js
@@ -0,0 +1,15 @@
+function transformToCamelCase(string) {
+    let array = string.split('');
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === '-') {
+            array[i + 1] = array[i + 1].toUpperCase();
+            array.splice(i, 1);
+        };
+    };
+
+    return array.join('');
+};
+
+let a = transformToCamelCase('background-color');
+console.log(a);
